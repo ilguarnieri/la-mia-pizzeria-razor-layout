ebcb75955e52edc5bae787704ec8a7341c66a771
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-07-17 19:22:04 +0200

    Aggiungere i file di progetto.

---
+++ Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Photo { get; private set; }
+        public float Price { get; private set; }
+
+        public Pizza(string name, string description, string photo, string price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Photo = photo;
+            this.Price = float.Parse(price);
+        }
+
+
+    }
+}
---
+++ Views/Home/Menu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace la_mia_pizzeria_static.Views.Home
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
